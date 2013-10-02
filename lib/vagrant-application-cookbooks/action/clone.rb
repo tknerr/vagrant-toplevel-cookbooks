a120549133d2f2a0d0a0878117fd20f0cd2ed56d
Torben <tkn@zuehlke.com>
2013-10-02 08:18:48 +0200

    initial working skeleton, added config option to specify the git ref to check out

--- lib/vagrant-application-cookbooks/action/clone.rb
+++ lib/vagrant-application-cookbooks/action/clone.rb
@@ -6,37 +6,72 @@ module VagrantPlugins
       # This middleware checks if reqiured plugins are present
       class Clone
 
+        attr_reader :cloned_repo_path, :cookbook_install_path, :git_url, :git_ref
+
         def initialize(app, env)
-          @app    = app
+          @app = app
+          @env = env
+          # by default data_dir points to ./.vagrant/machines/<NAME>/<PROVIDER>
+          @cloned_repo_path = env[:machine].data_dir.parent.join('app-cookbook', 'repo')
+          @cookbook_install_path = env[:machine].data_dir.parent.join('app-cookbook', 'cookbooks')
+          # from config
+          @git_url = env[:machine].config.app_cookbook.url
+          @git_ref = env[:machine].config.app_cookbook.ref
         end
 
-        def provisioners(name, env)
-          env[:machine].config.vm.provisioners.select{ |prov| prov.name == name }
+        def provisioners(name)
+          @env[:machine].config.vm.provisioners.select{ |prov| prov.name == name }
+        end
+
+        def log(msg)
+          @env[:ui].info msg
+        end
+
+        def clean_and_clone_repo
+          log "cleaning #{cloned_repo_path}"
+          FileUtils.rm_rf cloned_repo_path
+          FileUtils.mkdir_p cloned_repo_path
+
+          log "cloning application cookbook from #{git_url} into #{cloned_repo_path}"
+          system "git clone #{git_url} #{cloned_repo_path}"
+        end
+
+        def update_and_checkout
+          log "checking out ref #{git_ref} in #{cloned_repo_path}"
+          system "cd #{cloned_repo_path} && git pull && git checkout #{git_ref}"
+        end
+
+        def is_cloned
+          File.exist?(cloned_repo_path) && get_origin.eql?(git_url)
+        end
+
+        def get_origin
+          log "getting origin for #{cloned_repo_path}"
+          `cd #{cloned_repo_path} && git config --get remote.origin.url`.strip
+        end
+
+        def install_cookbooks
+          log "installing cookbook dependencies to #{cookbook_install_path}"
+          system "cd #{cloned_repo_path} && berks install --path #{cookbook_install_path}"
+        end
+
+        def configure_chef_solo
+          log "configuring chef_solo provisioners with cookbook_install_path = #{cookbook_install_path}"
+          provisioners(:chef_solo).each do |provisioner|
+            provisioner.config.cookbooks_path = provisioner.config.send(:prepare_folders_config, cookbook_install_path)
+          end
         end
 
         def call(env)
+          # ensure correct repo is cloned and ref checked out
+          clean_and_clone_repo unless is_cloned
+          update_and_checkout
 
-          url = env[:machine].config.app_cookbook.url
-          vm_name = env[:machine].name
-          target_path = ".vagrant/app_cookbooks/#{vm_name}/app"
-          cb_path = ".vagrant/app_cookbooks/#{vm_name}/cookbooks"
-
-          unless File.exist?(cb_path)
-            env[:ui].info "cleaning #{target_path}"
-            FileUtils.rm_rf target_path
-            FileUtils.mkdir_p target_path
-
-            env[:ui].info "cloning application cookbook from #{url} into #{target_path}"
-            system "git clone #{url} #{target_path}"
-
-            env[:ui].info "installing cookbook dependencies to #{cb_path}"
-            system "cd #{target_path} && berks install --path ../cookbooks"
-          end
-
-          env[:ui].info "configuring chef_solo provisioners with cookbooks_path = #{cb_path}"
-          provisioners(:chef_solo, env).each do |provisioner|
-            provisioner.config.cookbooks_path = provisioner.config.send(:prepare_folders_config, cb_path)
-          end
+          # install app cookbook dependencies
+          install_cookbooks
+          
+          # configure cookbooks_path
+          configure_chef_solo
 
           # continue if ok
           @app.call(env)
--- lib/vagrant-application-cookbooks/config.rb
+++ lib/vagrant-application-cookbooks/config.rb
@@ -3,20 +3,28 @@ require "vagrant"
 module VagrantPlugins
   module ApplicationCookbooks
     class Config < Vagrant.plugin(2, :config)
+      
+      # git url to clone from
       attr_accessor :url
+      # git ref / branch to checkout
+      attr_accessor :ref
 
       def initialize
         @url = UNSET_VALUE
+        @ref = UNSET_VALUE
       end
 
       def validate(machine)
         errors = _detected_errors
-        #errors << "url must not be nil" if @url.nil?
+        if @ref != nil
+          errors << "url must not be nil if ref is specified" if @url.nil?
+        end
         { "vagrant-application-cookbooks" => errors }
       end
 
       def finalize!
         @url = nil if @url == UNSET_VALUE
+        @ref = 'master' if @ref == UNSET_VALUE
       end
     end
   end
