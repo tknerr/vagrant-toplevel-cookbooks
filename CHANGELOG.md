a4bd715bc74b19b8154b94b2475958e3795a6966
Torben <tkn@zuehlke.com>
2013-10-05 03:08:35 +0200

    up version for development to 0.2.0.dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 
+# 0.1.3 (unreleased)
+
+* ...
+
 # 0.1.2 (Oct 5, 2013)
 
 * Silence output of git commands
--- lib/vagrant-application-cookbooks/version.rb
+++ lib/vagrant-application-cookbooks/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module ApplicationCookbooks
-    VERSION = "0.1.2"
+    VERSION = "0.2.0.dev"
   end
 end
